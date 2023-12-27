b01cfacf37c7d68c1796f83ced2bdb7bffe18721
billy4479 <giachi.ellero@gmail.com>
2023-12-27 21:07:18 +0100

    Add more browser settings, joplin and xdg open defaults

--- user/browser/firefox-based/default.nix
+++ user/browser/firefox-based/default.nix
@@ -22,10 +22,18 @@ let
     "browser.search.suggest.enabled" = true;
     "browser.urlbar.suggest.searches" = true;
 
+    # Fonts
+    "font.default.x-western" = "sans-serif";
+    "font.name.monospace.x-western" = "FiraCode Nerd Font";
+    "font.name.sans-serif.x-western" = "SF Pro Display";
+    "font.name.serif.x-western" = "SF Pro Display"; # Basically disable serif fonts
+    "browser.display.use_document_fonts" = 0; # No custom fonts
+
     "browser.aboutConfig.showWarning" = false;
     "browser.bookmarks.addedImportButton" = true;
     "browser.topsites.contile.cachedTiles" = "";
     "browser.uiCustomization.state" = builtins.readFile ./ui-state.json;
+    "services.sync.engine.passwords" = false;
   };
 in
 {
--- user/default.nix
+++ user/default.nix
@@ -43,7 +43,20 @@
     telegram-desktop
     spotify
     discord
+    joplin-desktop
   ];
+
+  xdg.mimeApps = {
+    enable = true;
+    associations.added = {
+      "x-scheme-handler/tg" = "org.telegram.desktop.desktop";
+      "application/pdf" = "org.pwmt.zathura.desktop";
+    };
+    defaultApplications = {
+      "x-scheme-handler/tg" = "org.telegram.desktop.desktop";
+      "application/pdf" = "org.pwmt.zathura.desktop";
+    };
+  };
 
   home.file = { };
 
