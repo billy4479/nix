f704a5a2c80a25f136f370b4767f93e2553dcd48
billy4479 <giachi.ellero@gmail.com>
2024-01-13 21:47:20 +0100

    Add night colors and window focus preferences in KDE

--- user/kde.nix
+++ user/kde.nix
@@ -2,6 +2,18 @@
   programs.plasma = {
     enable = true;
     configFile = {
+      "kwinrc" = {
+        "NightColor" = {
+          "Active" = true;
+          "Mode" = "Constant";
+          "NightTemperature" = 4900;
+        };
+        "Windows" = {
+          "DelayFocusInterval" = 100;
+          "FocusPolicy" = "FocusFollowsMouse";
+        };
+      };
+
       "kdeglobals"."KDE"."SingleClick" = false;
       "kcminputrc" = {
         # TODO: We don't want to hardcode the device name, see https://github.com/pjones/plasma-manager/issues/47
