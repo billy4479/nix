070038bdaa76065a81c71561d2527a0e760d8f18
billy4479 <giachi.ellero@gmail.com>
2024-01-26 23:53:37 +0100

    Use kate to open plain text

--- user/xdg-open.nix
+++ user/xdg-open.nix
@@ -5,10 +5,12 @@
     associations.added = {
       "x-scheme-handler/tg" = "org.telegram.desktop.desktop";
       "application/pdf" = "org.pwmt.zathura.desktop";
+      "text/plain" = "org.kde.kate.desktop";
     };
     defaultApplications = {
       "x-scheme-handler/tg" = "org.telegram.desktop.desktop";
       "application/pdf" = "org.pwmt.zathura.desktop";
+      "text/plain" = "org.kde.kate.desktop";
     };
   };
 }
