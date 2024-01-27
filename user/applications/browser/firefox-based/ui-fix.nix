1ac26aa91aea3c2989ab0b63416185848d752749
billy4479 <giachi.ellero@gmail.com>
2024-01-27 11:58:55 +0100

    fix firefox icons

--- user/applications/browser/firefox-based/ui-fix.nix
+++ user/applications/browser/firefox-based/ui-fix.nix
@@ -14,6 +14,8 @@
   # TODO: support librewolf too
   profilePath = ".mozilla/firefox/${extraConfig.user.username}";
 in {
+  home.file."${profilePath}/icons".source = "${srcs}/icons";
+
   programs.firefox.profiles.${extraConfig.user.username} = {
     extraConfig =
       "\n\n// --- Options from https://github.com/black7375/Firefox-UI-Fix ---\n\n "
