51a4b978f02ff2f511a291d570e1b280ee8fc4c8
billy4479 <giachi.ellero@gmail.com>
2024-05-20 16:30:37 +0200

    Add better-escape

--- user/applications/editor/nvim/user/init.lua
+++ user/applications/editor/nvim/user/init.lua
@@ -1,17 +1,23 @@
 return {
-    colorscheme = "catppuccin",
-  
-    plugins = {
-      {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        config = function()
-          require("catppuccin").setup {
-            flavour = "frappe",
-            transparent_background = true,
-          }
-        end,
-      },
-    },
-  }
-  
+	colorscheme = "catppuccin",
+
+	plugins = {
+		{
+			"catppuccin/nvim",
+			name = "catppuccin",
+			config = function()
+				require("catppuccin").setup({
+					flavour = "frappe",
+					transparent_background = true,
+				})
+			end,
+		},
+		{
+			"max397574/better-escape.nvim",
+			name = "better-escape.nvim",
+			config = function()
+				require("better_escape").setup()
+			end,
+		},
+	},
+}
