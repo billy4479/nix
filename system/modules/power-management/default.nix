3b7e98b4f6dd7b7bda3e73d3b24e789694bc7900
billy4479 <giachi.ellero@gmail.com>
2024-06-21 13:47:42 +0200

    Update flake

--- system/modules/power-management/default.nix
+++ system/modules/power-management/default.nix
@@ -1,6 +1,6 @@
 { ... }: {
   imports = [
-    ./auto-cpufreq.nix
+    # ./auto-cpufreq.nix # Apparently power-management-daemon is enabled by default now, we'll use that
     ./powertop.nix
   ];
 }
