f37adf27d302ecbb8cf0f5c6c1ac3815d4da5d4e
billy4479 <giachi.ellero@gmail.com>
2024-11-14 09:45:07 +0100

    Fix touchpad name
    Probably should look into the new way of configuring libinput devices

--- user/modules/desktops/kde/default.nix
+++ user/modules/desktops/kde/default.nix
@@ -21,7 +21,7 @@
 
       # TODO: We don't want to hardcode the device name, see https://github.com/pjones/plasma-manager/issues/47
       "kcminputrc" = {
-        "Libinput/10182/480/GXTP7863:00 27C6:01E0 Touchpad" = {
+        "Libinput/10182/480/GXTP7863:00 27C6:01E0" = {
           "DisableWhileTyping".value = false;
           "NaturalScroll".value = true;
           "TapToClick".value = true;
