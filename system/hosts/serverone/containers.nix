1606be435fd739fdd672da379053de4b580d09ec
billy4479 <giachi.ellero@gmail.com>
2024-12-23 17:47:56 +0100

    Finally configure wireguard properly to access containers

--- system/hosts/serverone/containers.nix
+++ system/hosts/serverone/containers.nix
@@ -8,7 +8,15 @@
     podman = {
       enable = true;
 
-      defaultNetwork.settings.dns_enabled = true;
+      defaultNetwork.settings = {
+        dns_enabled = true;
+        subnets = [
+          {
+            gateway = "10.0.1.1";
+            subnet = "10.0.1.0/24";
+          }
+        ];
+      };
     };
     oci-containers.backend = "podman";
   };
