2ed22404afe62c67f225edbe39101509c352892c
billy4479 <giachi.ellero@gmail.com>
2024-12-29 19:03:56 +0100

    Add lsp support in nvim!
    It wasn't that hard after all

--- user/modules/applications/editor/nvim/default.nix
+++ user/modules/applications/editor/nvim/default.nix
@@ -8,9 +8,11 @@
   imports = [
     ./plugins/catppuccin.nix
     ./plugins/format.nix
+    ./plugins/lsp.nix
     ./plugins/telescope.nix
     ./plugins/treesitter.nix
   ];
+
   programs.neovim = {
     enable = true;
     extraLuaConfig = # lua
@@ -40,24 +42,24 @@
 
         vim.g.mapleader = " "
         vim.keymap.set("i", "<C-c>", "<Esc>")
-        vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+        vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
-        vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-        vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-        vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-        vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+        vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+        vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+        vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+        vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
         vim.keymap.set("x", "<leader>p", [["_dP]])
-        vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+        vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
         vim.keymap.set("n", "<leader>Y", [["+Y]])
-        vim.keymap.set({"n", "v"}, "<leader>d", "\"_d")
+        vim.keymap.set({ "n", "v" }, "<leader>d", '"_d')
 
-        vim.api.nvim_create_autocmd('TextYankPost', {
-            desc = 'Highlight when yanking (copying) text',
-            group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-            callback = function()
-              vim.highlight.on_yank()
-            end,
+        vim.api.nvim_create_autocmd("TextYankPost", {
+        	desc = "Highlight when yanking (copying) text",
+        	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+        	callback = function()
+        		vim.highlight.on_yank()
+        	end,
         })
       '';
   };
