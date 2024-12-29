f71206e41313575d0173e2241e173cbab74abcc0
billy4479 <giachi.ellero@gmail.com>
2024-12-29 20:00:39 +0100

    Add linter

--- user/modules/applications/editor/nvim/plugins/lsp.nix
+++ user/modules/applications/editor/nvim/plugins/lsp.nix
@@ -129,5 +129,22 @@
           })
         '';
     }
+    {
+      plugin = nvim-lint;
+      type = "lua";
+      config = # lua
+        ''
+          require("lint").linters_by_ft = {
+          	go = { "golangcilint" },
+          }
+
+          vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+          	group = vim.api.nvim_create_augroup("custom-lint", { clear = true }),
+          	callback = function()
+          		require("lint").try_lint()
+          	end,
+          })
+        '';
+    }
   ];
 }
