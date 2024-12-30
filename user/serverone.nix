857c8a36c7632d7dd5dd32fcfb85aee02e8f7ffb
billy4479 <giachi.ellero@gmail.com>
2024-12-30 19:52:31 +0100

    Add direnv to serverone

--- user/serverone.nix
+++ user/serverone.nix
@@ -3,6 +3,7 @@
   imports = [
     # Include only a minimal set of applications
     ./modules/applications/btop.nix
+    ./modules/applications/direnv.nix
     ./modules/applications/editor/nvim
     ./modules/applications/git.nix
     ./modules/applications/shell
