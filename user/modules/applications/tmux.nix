863c28f173395fa4fd68553f80a81e9fcc6530c1
billy4479 <giachi.ellero@gmail.com>
2025-01-05 19:00:38 +0100

    Use tmux instead of zellij

---
+++ user/modules/applications/tmux.nix
@@ -0,0 +1,11 @@
+{ ... }:
+{
+  catppuccin.tmux.enable = true;
+  programs.tmux = {
+    enable = true;
+    historyLimit = 10000;
+    keyMode = "vi";
+    mouse = true;
+    shortcut = "a";
+  };
+}
