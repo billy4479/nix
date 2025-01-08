2c0e606e26058ad1f673876e78455d2bb9d3d2de
billy4479 <giachi.ellero@gmail.com>
2025-01-08 12:02:12 +0100

    Remove layouts I don't use

--- user/modules/desktops/qtile/config.py
+++ user/modules/desktops/qtile/config.py
@@ -210,32 +210,8 @@ layout_theme = {"border_width": 1,
 
 layouts = [
     layout.MonadWide(**layout_theme),
-    # layout.Bsp(**layout_theme),
-    # layout.Stack(stacks=2, **layout_theme),
-    # layout.Columns(**layout_theme),
     layout.RatioTile(**layout_theme),
-    # layout.VerticalTile(**layout_theme),
-    layout.Matrix(**layout_theme),
-    # layout.Zoomy(**layout_theme),
     layout.MonadTall(**layout_theme),
-    layout.Max(**layout_theme),
-    layout.Tile(shift_windows=False, **layout_theme),
-    # layout.Stack(num_stacks=2),
-    # layout.TreeTab(
-    #     font = "Ubuntu",
-    #     fontsize = 10,
-    #     sections = ["FIRST", "SECOND"],
-    #     section_fontsize = 11,
-    #     bg_color = "141414",
-    #     active_bg = "90C435",
-    #     active_fg = "000000",
-    #     inactive_bg = "384323",
-    #     inactive_fg = "a0a0a0",
-    #     padding_y = 5,
-    #     section_top = 10,
-    #     panel_width = 320
-    #     ),
-    layout.Floating(**layout_theme)
 ]
 
 
