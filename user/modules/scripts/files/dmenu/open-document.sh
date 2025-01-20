9df03e9902944d8f5ed265d280d723abb8d37cd3
billy4479 <giachi.ellero@gmail.com>
2025-01-20 11:15:41 +0100

    Oops

--- user/modules/scripts/files/dmenu/open-document.sh
+++ user/modules/scripts/files/dmenu/open-document.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 files=$(fd --extension=pdf -I . "$HOME/Downloads" "$HOME/code")
 files=$(echo "$files" | sed "s,$HOME/,,g")
-echo "$files" | rofi -dmenu | xargs --delimiter '\n' printf "$HOME/%s" | xargs xdg-open
+echo "$files" | rofi -dmenu | xargs --delimiter '\n' printf "$HOME/%s\n" | xargs --delimiter '\n' xdg-open
