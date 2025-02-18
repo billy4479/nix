7df68644cd5a280a7bf1bdb7303b9a0700df1912
billy4479 <giachi.ellero@gmail.com>
2025-02-18 16:15:46 +0100

    [desktops] scripts: move scripts in a nix file

--- user/modules/scripts/files/dmenu/open-document.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -euo pipefail
-
-files=$(fd --extension=pdf -I . "$HOME/Downloads" "$HOME/code")
-files=$(echo "$files" | sed "s,$HOME/,,g")
-echo "$files" | rofi -dmenu | xargs --delimiter '\n' printf "$HOME/%s\n" | xargs --delimiter '\n' xdg-open
