8c5488ce692490280c5efe34948337bdef8e5a4f
billy4479 <giachi.ellero@gmail.com>
2025-10-22 12:39:49 +0200

    jackett: use setCommonContainerConfig

--- containers/jackett.nix
+++ containers/jackett.nix
@@ -3,7 +3,7 @@ let
   containerName = "jackett";
   configDir = "/mnt/SSD/apps/${containerName}/config";
   downloadsDir = "/mnt/SSD/apps/${containerName}/downloads";
-  inherit (import ./utils.nix) givePermissions;
+  inherit (import ./utils.nix) givePermissions setCommonContainerConfig;
 in
 {
   virtualisation.oci-containers.containers."${containerName}" = {
@@ -17,14 +17,11 @@ in
       "${downloadsDir}:/downloads:rw"
       "${configDir}:/config:rw"
     ];
-
-    labels = {
-      "io.containers.autoupdate" = "registry";
-    };
-
-    extraOptions = [ "--ip=10.0.1.8" ];
-  };
-
+  }
+  // (setCommonContainerConfig {
+    ip = "10.0.1.8";
+    runByUser = false;
+  });
 }
 // (givePermissions {
   inherit pkgs containerName;
