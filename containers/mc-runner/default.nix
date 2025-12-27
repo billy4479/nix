e71036fb6e77709bbce94c92cf5bbc5f839fa38c
billy4479 <giachi.ellero@gmail.com>
2025-12-27 16:23:48 +0100

    mc-runner: init

---
+++ containers/mc-runner/default.nix
@@ -0,0 +1,35 @@
+{
+  pkgs,
+  config,
+  extraPkgs,
+  ...
+}:
+let
+  inherit ((import ../utils.nix) { inherit pkgs config; }) makeContainer;
+
+  name = "mc-runner";
+  baseDir = "/mnt/SSD/apps/${name}";
+  worldDir = "/mnt/SSD/minecraft/test-world";
+in
+makeContainer {
+  inherit name;
+  image = "localhost/mc-runner:latest";
+  imageFile = extraPkgs.my-packages.containers.mc-runner;
+  ip = "10.0.1.13";
+
+  environment = {
+    DONT_LOAD_DOTENV = "yes";
+    PORT = "4479";
+    VITE_PORT = "5173";
+    ENVIRONMENT = "debug";
+    CONFIG_PATH = "/mc-runner/config.yml";
+  };
+
+  volumes = [
+    "${baseDir}:/mc-runner:rw"
+    "${worldDir}:/world:rw"
+    "${./config.yml}:/mc-runner/config.yml:ro"
+  ];
+  adminOnlyDirs = [ baseDir ];
+  userDirs = [ worldDir ];
+}
