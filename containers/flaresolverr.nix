07888aa7560619113a8fc8aa35a34aba9a0c181a
billy4479 <giachi.ellero@gmail.com>
2026-01-13 21:51:00 +0100

    flaresolverr: fix some stuff
    apparently still doesn't work but it is not my fault now.

--- containers/flaresolverr.nix
+++ containers/flaresolverr.nix
@@ -4,33 +4,53 @@ let
   baseSSDDir = "/mnt/SSD/apps/${name}";
 in
 {
+  # https://chatgpt.com/share/e/6966ac17-7d2c-8014-9a54-ab1ca711c179
   nerdctl-containers.${name} = {
     imageToBuild = pkgs.nix-snapshotter.buildImage {
       inherit name;
       tag = "nix-local";
 
       config = {
-        Env = [ "HOME=/app" ];
-        EntryPoint = [ (lib.getExe pkgs.flaresolverr) ];
+        Env = [
+          "HOME=/app"
+          "XDG_CACHE_HOME=/tmp"
+          "FONTCONFIG_FILE=${pkgs.fontconfig.out}/etc/fonts/fonts.conf"
+        ];
+        EntryPoint = [
+          (pkgs.writeShellScript "entrypoint.sh" ''
+            set -e
+            mkdir -p /tmp/.X11-unix
+            chmod 1777 /tmp/.X11-unix
+            exec ${lib.getExe pkgs.flaresolverr}
+          '')
+        ];
         WorkingDir = "/app";
       };
+
+      copyToRoot = with pkgs; [
+        dockerTools.caCertificates
+
+        xorg.xvfb
+        coreutils
+        fontconfig
+        noto-fonts
+      ];
     };
     id = 133;
 
     environment = {
-      "LOG_LEVEL" = "info";
+      "LOG_LEVEL" = "debug";
     };
 
     volumes = [
       {
-        hostPath = "${baseSSDDir}/local";
-        containerPath = "/app/.local";
+        hostPath = "${baseSSDDir}";
+        containerPath = "/app";
       }
     ];
 
     tmpfs = [
       "/tmp"
-      "/app/.cache"
     ];
   };
 }
