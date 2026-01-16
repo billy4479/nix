c7a065cbbc1f407db16cc109978b70a5818cb162
billy4479 <giachi.ellero@gmail.com>
2026-01-16 12:38:06 +0100

    adb: remove deprecated option

--- system/modules/desktops/default.nix
+++ system/modules/desktops/default.nix
@@ -38,10 +38,11 @@ in
 
   security.polkit.enable = true;
 
+  environment.systemPackages = [ pkgs.android-tools ];
+
   programs = {
     # https://discourse.nixos.org/t/error-gdbus-error-org-freedesktop-dbus-error-serviceunknown-the-name-ca-desrt-dconf-was-not-provided-by-any-service-files/29111/2
     dconf.enable = true;
-    adb.enable = true;
   };
 
 }
