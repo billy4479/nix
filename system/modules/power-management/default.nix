dccfa4c1ead5e21fa70aee21750e29d54d599fe1
billy4479 <giachi.ellero@gmail.com>
2026-02-23 12:14:38 +0100

    power-management: add components for noctalia

--- system/modules/power-management/default.nix
+++ system/modules/power-management/default.nix
@@ -4,4 +4,7 @@
     # ./auto-cpufreq.nix # Apparently power-management-daemon is enabled by default now, we'll use that
     ./powertop.nix
   ];
+
+  services.power-profiles-daemon.enable = true;
+  services.upower.enable = true;
 }
