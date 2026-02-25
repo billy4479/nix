d3d110210c51e989be12f13055afaf2e040b68e9
billy4479 <giachi.ellero@gmail.com>
2026-02-25 16:04:26 +0100

    sops: verify sops file

--- user/modules/secrets.nix
+++ user/modules/secrets.nix
@@ -7,6 +7,6 @@
   sops = {
     defaultSopsFile = "${flakeInputs.secrets-repo}/${extraConfig.hostname}.yaml";
     age.keyFile = "/var/lib/sops-nix/key.txt";
-    validateSopsFiles = false;
+    validateSopsFiles = true;
   };
 }
