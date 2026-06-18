9c9722305536047daaee81181af7e1fab0c48545
billy4479 <giachi.ellero@gmail.com>
2026-06-18 17:45:26 +0200

    opencloud: build from nixpkgs

--- containers/opencloud.nix
+++ containers/opencloud.nix
@@ -1,6 +1,10 @@
-{ config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 let
-
   name = "opencloud";
   domain = "opencloud.polpetta.online";
 
@@ -13,18 +17,39 @@ in
   sops.secrets.opencloud-env = { };
 
   nerdctl-containers.${name} = {
-    imageToPull = "docker.io/opencloudeu/opencloud-rolling";
+    imageToBuild = pkgs.nix-snapshotter.buildImage {
+      inherit name;
+      tag = "nix-local";
+
+      copyToRoot = with pkgs; [
+        dockerTools.caCertificates
+        dockerTools.binSh
+        opencloud
+      ];
+
+      config = {
+        entrypoint = [ "/bin/sh" ];
+        env = [
+          "WEB_ASSET_CORE_PATH=${pkgs.opencloud.web}"
+          "IDP_ASSET_PATH=${pkgs.opencloud.idp-web}/assets"
+          "OC_CONFIG_DIR=/etc/opencloud"
+          "OC_BASE_DATA_PATH=/var/lib/opencloud"
+        ];
+      };
+    };
+
     id = 14;
     useNginx = true;
 
     environment = {
+      # These are awfully undocumented, some are here but the list is incomplete
+      # https://docs.opencloud.eu/docs/dev/server/configuration/global-environment-variables
       "OC_INSECURE" = "false";
       "OC_URL" = "https://${domain}";
       "PROXY_HTTP_ADDR" = "10.0.1.14:9200";
       "PROXY_TLS" = "false";
 
       "FRONTEND_ARCHIVER_MAX_SIZE" = "10000000000";
-      "FRONTEND_CHECK_FOR_UPDATES" = "true";
     };
 
     environmentFiles = [ config.sops.secrets.opencloud-env.path ];
