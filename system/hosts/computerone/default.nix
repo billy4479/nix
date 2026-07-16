c7b6f952b495462be1af4f20de62d6015997dccf
billy4479 <giachi.ellero@gmail.com>
2026-07-16 12:04:33 +0200

    computerone: add printer support

--- system/hosts/computerone/default.nix
+++ system/hosts/computerone/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./hardware-configuration.nix
@@ -16,6 +16,33 @@
   ];
 
   networking.hostName = "computerone";
+
+  systemd.services.configure-hp-laserjet-mfp-m28w = {
+    description = "Configure HP LaserJet MFP M28w CUPS queue";
+    after = [
+      "cups.service"
+      "network-online.target"
+    ];
+    wants = [
+      "cups.service"
+      "network-online.target"
+    ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig.Type = "oneshot";
+    script = # sh
+      ''
+        ${pkgs.cups}/bin/lpadmin \
+          -p hp-laserjet-mfp-m28w \
+          -D "HP LaserJet MFP M28w" \
+          -E \
+          -v ipp://192.168.2.180/ipp/print \
+          -m everywhere
+        ${pkgs.cups}/bin/cupsenable hp-laserjet-mfp-m28w
+        ${pkgs.cups}/bin/cupsaccept hp-laserjet-mfp-m28w
+        ${pkgs.cups}/bin/lpoptions -d hp-laserjet-mfp-m28w
+      '';
+  };
 
   # FIXME: Re-enable ZFS when zfs-kernel is no longer marked broken in nixpkgs.
   # boot.supportedFilesystems = [ "zfs" ];
