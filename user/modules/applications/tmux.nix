7df7da64bc416cabb6e924fd65b4f269b6cb8a50
billy4479 <giachi.ellero@gmail.com>
2026-08-18 16:24:21 +0200

    tmux: attach on ssh

--- user/modules/applications/tmux.nix
+++ user/modules/applications/tmux.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 {
   catppuccin.tmux.enable = true;
   programs.tmux = {
@@ -8,4 +8,19 @@
     mouse = true;
     shortcut = "b";
   };
+
+  programs.zsh.initContent =
+    lib.mkAfter # sh
+      ''
+        if [[ -n "$SSH_CONNECTION" && -z "$TMUX" && -t 0 ]]; then
+          exec tmux new-session -A -s ssh
+        fi
+      '';
+
+  programs.bash.initExtra = # sh
+    ''
+      if [[ -n "$SSH_CONNECTION" && -z "$TMUX" && -t 0 ]]; then
+        exec tmux new-session -A -s ssh
+      fi
+    '';
 }
