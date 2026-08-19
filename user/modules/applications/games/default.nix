4c4458629f700562266c6b1f35841ae046bb568e
billy4479 <giachi.ellero@gmail.com>
2026-08-19 18:47:30 +0200

    games: add protonup-qt

--- user/modules/applications/games/default.nix
+++ user/modules/applications/games/default.nix
@@ -8,7 +8,10 @@
   imports = [ ./minecraft ];
 
   config = lib.mkIf extraConfig.games {
-    home.packages = with pkgs; [ wineWow64Packages.staging ];
+    home.packages = with pkgs; [
+      wineWow64Packages.staging
+      protonup-qt
+    ];
 
     programs.mangohud.enable = true;
 
